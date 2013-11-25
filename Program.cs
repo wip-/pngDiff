5ade229347ada0494b69d5f83161da9529dbd871
Wil <wil@server.fake>
2013-11-25 14:16:31 +0900

    Compare color palettes

--- Program.cs
+++ Program.cs
@@ -69,70 +69,132 @@ namespace pngDiff
                 ImageLockMode.ReadOnly,
                 bitmap1.PixelFormat);
 
-            int bitmapStride = bitmapData0.Stride;
+            int bitmapStride0 = bitmapData0.Stride;
+            int bitmapStride1 = bitmapData1.Stride;
 
-            if (bitmapStride != bitmapData1.Stride)
+            if (bitmapStride0 != bitmapStride1)
             {
-                Console.WriteLine("The files strides are different. {0} vs {1}",
-                    bitmapStride, bitmapData1.Stride);
-                return;
+                Console.WriteLine("Note: The files strides are different. {0} vs {1}",
+                    bitmapStride0, bitmapStride1);
             }
 
             if (bitmap0.PixelFormat != bitmap1.PixelFormat)
             {
-                Console.WriteLine("The files formats are different. {0} vs {1}",
+                Console.WriteLine("Note: The files formats are different. {0} vs {1}",
                     bitmap0.PixelFormat.ToString(), bitmap1.PixelFormat.ToString());
-                return;
             }
 
-            int bitmapComponents = GetComponentsNumber(bitmap0.PixelFormat);
+            int bitmapComponents0 = GetComponentsNumber(bitmap0.PixelFormat);
+            int bitmapComponents1 = GetComponentsNumber(bitmap1.PixelFormat);
+
             int diffPixels = 0;
 
             Console.WriteLine();
             Console.WriteLine(path0);
             Console.WriteLine(path1);
 
-            if (bitmap0.PixelFormat == PixelFormat.Format8bppIndexed)
-            {
-                for (int y = 0; y <= bitmapHeight - 1; y++)
-                {
-                    for (int x = 0; x <= bitmapWidth - 1; x++)
-                    {
-                        byte index0 = Marshal.ReadByte(bitmapData0.Scan0, (bitmapStride * y) + (bitmapComponents * x));
-                        byte index1 = Marshal.ReadByte(bitmapData1.Scan0, (bitmapStride * y) + (bitmapComponents * x));
-                        if (index0 != index1)
-                        {
-                            ++diffPixels;
-                        }
-                    }
-                }
-            }
-            else
-            {
-                for (int y = 0; y <= bitmapHeight - 1; y++)
-                {
-                    for (int x = 0; x <= bitmapWidth - 1; x++)
-                    {
-                        Color color0 = Color.FromArgb(
-                            Marshal.ReadInt32(bitmapData0.Scan0, (bitmapStride * y) + (bitmapComponents * x)));
-                        Color color1 = Color.FromArgb(
-                            Marshal.ReadInt32(bitmapData1.Scan0, (bitmapStride * y) + (bitmapComponents * x)));
+            var colorDictionary0 = new Dictionary<Color, int>();
+            var colorDictionary1 = new Dictionary<Color, int>();
 
-                        if (color0.ToArgb() != color1.ToArgb())
-                        {
-                            ++diffPixels;
-                        }
+            for (int y = 0; y < bitmapHeight; y++)
+            {
+                for (int x = 0; x < bitmapWidth; x++)
+                {
+                    Color color0 = GetColor(bitmapData0, x, y, bitmap0.PixelFormat, bitmapStride0, bitmapComponents0, bitmap0);
+                    Color color1 = GetColor(bitmapData1, x, y, bitmap1.PixelFormat, bitmapStride1, bitmapComponents1, bitmap1);
 
+                    if (color0.ToArgb() != color1.ToArgb())
+                    {
+                        ++diffPixels;
                     }
+
+                    if (colorDictionary0.ContainsKey(color0))
+                        colorDictionary0[color0] += 1;
+                    else
+                        colorDictionary0[color0] = 1;
+
+
+                    if (colorDictionary1.ContainsKey(color1))
+                        colorDictionary1[color1] += 1;
+                    else
+                        colorDictionary1[color1] = 1;
                 }
             }
 
             bitmap0.UnlockBits(bitmapData0);
             bitmap1.UnlockBits(bitmapData1);
 
-            Console.WriteLine("Number of different pixels: {0}", diffPixels);
+            Console.WriteLine("Number of pixels: {0}", bitmapWidth * bitmapHeight);
+            Console.WriteLine("Number of different pixels: {0} ({1}%)", diffPixels, 100 * (float)diffPixels / (bitmapWidth*bitmapHeight));
+            Console.WriteLine();
+
+            if (colorDictionary0.Keys.Count != colorDictionary1.Keys.Count)
+            {
+                Console.WriteLine("Color palette sizes differ: {0} vs {1}", colorDictionary0.Keys.Count, colorDictionary1.Keys.Count);
+            }
+            
+            //else
+
+            {
+                int diffColorPalette = 0;
+                foreach (Color color0 in colorDictionary0.Keys)
+                {
+                    if (!colorDictionary1.ContainsKey(color0))
+                    {
+                        ++diffColorPalette;
+                    }
+                }
+
+                if (diffColorPalette > 0)
+                {
+                    Console.WriteLine("Number of colors: {0}", colorDictionary0.Keys.Count);
+                    Console.WriteLine("Number of different colors: {0} ({1}%)", diffColorPalette, 100 * (float)diffColorPalette / colorDictionary0.Keys.Count);
+                }
+                else
+                {
+                    int diffColorDistribution = 0;
+
+                    foreach (Color color0 in colorDictionary0.Keys)
+                    {
+                        if (colorDictionary0[color0] != colorDictionary1[color0])
+                        {
+                            ++diffColorDistribution;
+                        }
+                    }
+
+
+                    Console.WriteLine("Number of colors: {0}", colorDictionary0.Keys.Count);
+                    Console.WriteLine("Number of colors with different distribution: {0} ({1}%)", diffColorDistribution, 100 * (float)diffColorDistribution / colorDictionary0.Keys.Count);
+                }
+
+            }
 
         }
+
+
+
+
+
+
+        static private Color GetColor(BitmapData bitmapData, int x, int y, PixelFormat pixelFormat, int bitmapStride, int bitmapComponents,
+            Bitmap bitmap)
+        {
+            if (pixelFormat == PixelFormat.Format8bppIndexed)
+            {
+                byte index = Marshal.ReadByte(bitmapData.Scan0, (bitmapStride * y) + (bitmapComponents * x));
+                return bitmap.Palette.Entries[index];
+            }
+            else
+            {
+                Color color = Color.FromArgb(
+                            Marshal.ReadInt32(bitmapData.Scan0, (bitmapStride * y) + (bitmapComponents * x)));
+                return color;
+            }
+        }
+
+
+
+
 
 
         static private int GetComponentsNumber(PixelFormat pixelFormat)
